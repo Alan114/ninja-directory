5f5ff54d4c45ac8d0f156294ae4935044b58a880
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-13 16:23:23 +0200

    property binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = 'Welcome to the home page';
+  myString = 'I love space';
+  myBoolean = true;
   constructor() {}
 
   ngOnInit(): void {}
